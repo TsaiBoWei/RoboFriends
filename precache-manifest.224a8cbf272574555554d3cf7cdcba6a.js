71cd8c6b321af5bf155010ef3dc5c39039697b78
Tsai Bo Wei(Brian) <eliot9010@gmail.com>
2020-05-28 16:48:13 +0800

    Updates

---
+++ precache-manifest.224a8cbf272574555554d3cf7cdcba6a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db1110b36dd004e5e1b522a262a112c4",
+    "url": "/TsaiPoWei/TsaiPoWei.github.io/index.html"
+  },
+  {
+    "revision": "5175959f1a4b3892227e",
+    "url": "/TsaiPoWei/TsaiPoWei.github.io/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7bfcaf3a2fb251fd67f6",
+    "url": "/TsaiPoWei/TsaiPoWei.github.io/static/css/main.8ba0d7b3.chunk.css"
+  },
+  {
+    "revision": "5175959f1a4b3892227e",
+    "url": "/TsaiPoWei/TsaiPoWei.github.io/static/js/2.719c2daa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TsaiPoWei/TsaiPoWei.github.io/static/js/2.719c2daa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bfcaf3a2fb251fd67f6",
+    "url": "/TsaiPoWei/TsaiPoWei.github.io/static/js/main.6ed54c7f.chunk.js"
+  },
+  {
+    "revision": "604ac69c68b1c7eb981f",
+    "url": "/TsaiPoWei/TsaiPoWei.github.io/static/js/runtime-main.b1822df4.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/TsaiPoWei/TsaiPoWei.github.io/static/media/SEGA.12a3db8b.woff"
+  }
+]);
